d4c6128776adf2b02040e376c40febda497d6d58
meldeleon <melissadeleon@gmail.com>
2022-05-18 22:58:37 -0700

    starting with discord.js set up

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const { REST } = require("@discordjs/rest")
-const { Route } = require("discord-api-types/v9")
+require("dotenv").config()
 
-const { commands } = require("./commands.js")
+// Require the necessary discord.js classes
+const { Client, Intents } = require("discord.js")
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!")
+})
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN)
---
+++ deploy-commands.js
@@ -0,0 +1,5 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { REST } = require("@discordjs/rest")
+const { Routes } = require("discord-api-types/v9")
+
+const commands = []
